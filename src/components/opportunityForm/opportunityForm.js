acb582342648fc61a75c17c73e3b605ec4dd44d8
winniekariuki <winniekariuki07@gmail.com>
2021-01-30 01:42:44 +0300

    add opportunity form

--- src/components/opportunityForm/opportunityForm.js
+++ src/components/opportunityForm/opportunityForm.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
+import { organisationActions } from "../../actions/organisation.action.js";
 import { opportunityActions } from "../../actions/opportunity.actions";
-import { organisationActions } from "../../actions/organisation.action";
-export default class OpportunityForm extends React.Component {
+class OpportunityForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       opportunity: {
         name: "",
         account: "",
-        amountStage: "",
-        submitted: false,
+        amountStage: "coconut",
       },
-      accounts: [],
-      amountStage: [],
+      value: 'coconut',
+      submitted: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -23,23 +22,43 @@ export default class OpportunityForm extends React.Component {
   componentDidMount() {
     this.props.getOrganisation();
   }
-
-  handleChange(e) {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
+  handleChange(event) {
+    const { name, value } = event.target;
+    const { opportunity } = this.state;
+    this.setState({
+      opportunity: {
+        ...opportunity,
+        [name]: value,
+      },
+    });
   }
+  
 
-  handleSubmit(e) {
-    e.preventDefault();
+  handleSubmit(event) {
+    event.preventDefault();
+
     this.setState({ submitted: true });
     const { opportunity } = this.state;
-    if (opportunity) {
+    if (opportunity.name && opportunity.account && opportunity.amountStage) {
       this.props.create(opportunity);
     }
   }
+  handleDropDown = (event) => {
+    const { name, value } = event.target;
+    const { opportunity } = this.state;
+    this.setState({
+      opportunity: {
+        ...opportunity,
+        [name]: value,
+      },
+    });
+  };
+
+
 
   render() {
     const { creating, organisation } = this.props;
+    const amountStageNames = [{"id":"1","name":"Discovery"},{"id":"2","name":"Proposal Shared"},{"id":"3","name":"Negotiations"}];
     const { opportunity, submitted } = this.state;
     return (
       <div className="col-md-6 col-md-offset-3">
@@ -50,7 +69,7 @@ export default class OpportunityForm extends React.Component {
               (submitted && !opportunity.name ? " has-error" : "")
             }
           >
-            <label htmlFor="name">Name</label>
+            <label htmlFor="name">Opportunity Name</label>
             <input
               type="text"
               className="form-control"
@@ -68,17 +87,20 @@ export default class OpportunityForm extends React.Component {
               (submitted && !opportunity.account ? " has-error" : "")
             }
           >
-            <label>
-              Accounts:
-              <select value={this.state.value} onChange={this.handleChange}>
-                {organisation.items &&
-                  organisation.items.map((organisation) => (
-                    <option key={organisation._id} value={organisation.name}>
-                      {organisation.name}
-                    </option>
-                  ))}
-              </select>
-            </label>
+            <label htmlFor="address">account</label>
+            <select
+              className="dropdown"
+              name="account"
+              value={opportunity.account}
+              onChange={this.handleDropDown}
+            >
+              {organisation.items &&
+                organisation.items.map((organisation) => (
+                  <option key={organisation._id} value={organisation.name}>
+                    {organisation.name}
+                  </option>
+                ))}
+            </select>
             {submitted && !opportunity.account && (
               <div className="help-block">account is required</div>
             )}
@@ -89,22 +111,21 @@ export default class OpportunityForm extends React.Component {
               (submitted && !opportunity.amountStage ? " has-error" : "")
             }
           >
-            <label>
-              Amount Stage:
-              <select value={this.state.value} onChange={this.handleChange}>
-                <option value="grapefruit">Grapefruit</option>
-                <option value="lime">Lime</option>
-                <option value="coconut">Coconut</option>
-                <option value="mango">Mango</option>
-              </select>
-            </label>
+             <label>
+          Pick your favorite flavor:
+          <select value={organisation.amountStage} onChange={this.handleChange}>
+            <option value={organisation.amountStage}>Grapefruit</option>
+            <option value={organisation.amountStage}>coconut</option>
+            <option value={organisation.amountStage}>Mango</option>
+            <option value={organisation.amountStage} >Lime</option>
+          </select>
+        </label>
             {submitted && !opportunity.amountStage && (
-              <div className="help-block">ammount stage is required</div>
+              <div className="help-block">amount stage is required</div>
             )}
           </div>
-
           <div className="form-group">
-            <button className="btn btn-primary">Create Opportunity</button>
+            <button className="btn btn-primary">Create Account</button>
             {creating && (
               // eslint-disable-next-line jsx-a11y/alt-text
               <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
@@ -117,14 +138,14 @@ export default class OpportunityForm extends React.Component {
 }
 
 function mapState(state) {
-  const {  organisation } = state;
-  const { creating } = state.opportunity;
-  return { creating, organisation };
+  const { users, authentication, organisation, creating } = state;
+  const { user } = authentication;
+  return { user, users, organisation, creating };
 }
 
 const actionCreators = {
   create: opportunityActions.create,
-  getOrganisation: organisationActions.getAll
+  getOrganisation: organisationActions.getAll,
 };
 
 const connectedOpportunityForm = connect(
--- src/pages/OpportunitiesPage/OpportunitiesPage.jsx
+++ src/pages/OpportunitiesPage/OpportunitiesPage.jsx
@@ -1,56 +1,87 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import Opportunity from '../../Opportunity/Opportunity';
+import React from "react";
+// import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import Opportunity from "../../Opportunity/Opportunity";
 
-import {CardColumns, Container} from 'react-bootstrap';
-import { userActions } from '../../actions/user.actions';
-import { opportunityActions } from '../../actions/opportunity.actions';
+import { CardColumns, Container } from "react-bootstrap";
+import { userActions } from "../../actions/user.actions";
+import { opportunityActions } from "../../actions/opportunity.actions";
+import { organisationActions } from "../../actions/organisation.action";
+import { Modal } from "../../components/Modal/modal";
+import { OpportunityForm }  from "../../components/opportunityForm/opportunityForm";
 
 class OpportunitiesPage extends React.Component {
-    componentDidMount() {
-        this.props.getUsers();
-        this.props.getOpportunities();
-    }
+  constructor(props) {
+    super(props);
+    console.log(this.props,"<>><>props")
+    this.state = {
+      show: false,
+    };
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+  }
 
-    handleDeleteUser(id) {
-        return (e) => this.props.deleteUser(id);
-    }
+  showModal = () => {
+    this.setState({ show: true });
+  };
 
-    render() {
-        const { opportunity } = this.props;
-        console.log("<>>props",opportunity.items);
-      
-        const opportunityItem = opportunity.items && opportunity.items.map(opportunity => (
-          <Opportunity
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+
+  componentDidMount() {
+    this.props.getUsers();
+    this.props.getOpportunities();
+  }
+
+  handleDeleteUser(id) {
+    return (e) => this.props.deleteUser(id);
+  }
+
+  render() {
+    const { opportunity } = this.props;
+    console.log("<>>props", opportunity.items);
+
+    const opportunityItem =
+      opportunity.items &&
+      opportunity.items.map((opportunity) => (
+        <Opportunity
           key={opportunity._id}
-            name={opportunity.name}
-            account={opportunity.account}
-            amountStage={opportunity.amountStage}
-            
-          />
-      
-        ));
-        
-        return (
-            <Container>
-                <CardColumns>{opportunityItem}</CardColumns>
-               
-                </Container>
-        );
-    }
+          name={opportunity.name}
+          account={opportunity.account}
+          amountStage={opportunity.amountStage}
+        />
+      ));
+
+    return (
+      <Container>
+        <Modal show={this.state.show} handleClose={this.hideModal}>
+          <OpportunityForm />
+        </Modal>
+        <button type="button" class="btn" onClick={this.showModal}>
+          <i class="fas fa-plus"></i>Add Opportunity
+        </button>
+        <CardColumns>{opportunityItem}</CardColumns>
+      </Container>
+    );
+  }
 }
 
 function mapState(state) {
-    const { users, authentication,opportunity } = state;
-    const { user } = authentication;
-    return { user, users, opportunity };
+  const { users, authentication, opportunity,organisation } = state;
+  const { user } = authentication;
+  return { user, users, opportunity, organisation };
 }
 
 const actionCreators = {
-    getUsers: userActions.getAll,
-    getOpportunities: opportunityActions.getAll,
-}
+  getUsers: userActions.getAll,
+  getOpportunities: opportunityActions.getAll,
+  getOrganisation: organisationActions.getAll,
+  createOpportunities: opportunityActions.create,
+};
 
-const connectedOpportunitiesPage = connect(mapState, actionCreators)(OpportunitiesPage);
+const connectedOpportunitiesPage = connect(
+  mapState,
+  actionCreators
+)(OpportunitiesPage);
 export { connectedOpportunitiesPage as OpportunitiesPage };
